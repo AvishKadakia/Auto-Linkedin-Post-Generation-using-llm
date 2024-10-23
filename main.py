d927cce00d133d7183edb609d8cee423ebc89b7a
Avish Kadakia <avishkadakia1996@gmail.com>
2024-10-23 17:44:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,11 +58,11 @@ logger = logging.getLogger(__name__)
 # Constants
 SIMILARITY_THRESHOLD = 0.03  # Adjust as needed
 # Assign weights (preference to publish date)
-weight_date = 0.3
-weight_similarity = 0.7
+weight_date = 0.4
+weight_similarity = 0.6
 KEYWORD_COUNT = 10
 HASHTAG_COUNT = 7
-no_of_articles_to_publish = 2
+no_of_articles_to_publish = 10
 # Load NLP models globally
 nlp = spacy.load("en_core_web_md")
 nlp.add_pipe("textrank")
